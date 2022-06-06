d5e706fcf3fc9b27519ad4ac2f61f687c9af059a
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-06 10:02:08 -0400

    Atualizações

--- home/login.php
+++ home/login.php
@@ -1,9 +1,4 @@
 <style>
-    html,
-    body {
-    height: 100%;
-    width: 100%;
-    }
 
     body {
     display: flex;
@@ -34,28 +29,32 @@
     border-top-right-radius: 0;
     }
 </style>
-<div class="text-center">
-    <main class="form-signin w-100 m-auto">
-    <form>
-        <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
 
-        <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email address</label>
-        </div>
-        <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Password</label>
-        </div>
+        <main class="form-signin w-100 m-auto">
+        <form>
+            <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-        <div class="checkbox mb-3">
-        <label>
-            <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-    </form>
-    </main>
+            <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+        </form>
+        </main>
+
+    </div>
 </div>
