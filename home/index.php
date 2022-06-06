3cf686e8f0494f6a43e8ea0d8cfc8e33ab1488e1
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-06 11:24:18 -0400

    Atualizações

--- home/index.php
+++ home/index.php
@@ -1,7 +1,32 @@
+<?php
+    include("../lib/config/includes.php");
+
+    $query = "select * from tarefas order by data desc limit 100";
+    $result = mysqli_query($con, $query);
+?>
 <div class="container">
     <div class="row">
         <div clas="col">
-            Aqui começa toda a história
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Local</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while($d = mysqli_fetch_object($result)){
+                    ?>
+                    <tr>
+                        <td><?=$d->local?></td>
+                        <td><?=$d->data?></td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
 
--- lib/config/connect.php
+++ lib/config/connect.php
@@ -1,3 +1,3 @@
 <?php
     include("appinc/AppIncludes.php");
-    AppConnect();
+    $con = AppConnect();
