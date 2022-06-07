c8e31458db72773ac5bf10bd8f62d923c73942ab
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-06 22:19:01 -0400

    Atualizações

--- lib/config/fn.php
+++ lib/config/fn.php
@@ -4,7 +4,7 @@
 
         $path = "../../../sisbk/versoes/".$l;
         $diretorio = dir($path);
-         while($arquivo = $diretorio -> read()){
+         while($arquivo = $diretorio->read()){
         //     if($arquivo != '.' and $arquivo != '..'){
         //         echo "<a href='".$path.$arquivo."'>".$arquivo."</a><br />";
         //     }
