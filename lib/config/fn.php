7561c3e80ff158d52319839199d06e7441bb37d8
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-06 22:55:42 -0400

    Atualizações

--- lib/config/fn.php
+++ lib/config/fn.php
@@ -6,7 +6,7 @@
         $diretorio = dir($path);
          while($arquivo = $diretorio->read()){
             if($arquivo != '.' and $arquivo != '..'){
-                echo "<li><a class=\"dropdown-item\" href=\"../sisbk/versoes/".$l.'/'.$arquivo."\">".$arquivo."</a></li>";
+                echo "<li><a class=\"dropdown-item\" href=\"../sisbk/versoes/".$l.'/'.$arquivo."\" target='_blank'>Versão em ".date("d/m/Y H:i:s", filemtime($path.$arquivo))."</a></li>";
             }
          }
          $diretorio -> close();
