5d6216657dc00048ffcebcb10cab4b99a3aeb538
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-06 22:17:32 -0400

    Atualizações

--- lib/config/fn.php
+++ lib/config/fn.php
@@ -2,14 +2,14 @@
 
     function Versoes($l){
 
-        // $path = "../../../sisbk/versoes/".$l;
-        // $diretorio = dir($path);
+        $path = "../../../sisbk/versoes/".$l;
+        $diretorio = dir($path);
         // while($arquivo = $diretorio -> read()){
         //     if($arquivo != '.' and $arquivo != '..'){
         //         echo "<a href='".$path.$arquivo."'>".$arquivo."</a><br />";
         //     }
         // }
         // $diretorio -> close();
-        echo "ok";
+
 
     }
